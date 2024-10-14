a03f81101d413281551a6c6aa926649a5b06d780
Lucas Xavier <131720296+lucagx@users.noreply.github.com>
2024-10-14 13:37:47 -0300

    feat: configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 import DigimonList from './components/DigimonList'
-import digimon from './assets/digimon.svg';
+import digimonLogo from './assets/digimon.svg';
 
 const App: React.FC = () => {
   return (
       <div className='App'>
-        <img src={digimon} alt="logo-digimon" className="logo"/>
+        <img src={digimonLogo} alt="logo-digimon" className="logo"/>
         <h1>DigiFinder</h1>
         <DigimonList />        
       </div>
